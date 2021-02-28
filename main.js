f947bbbbbfa79d6327546396809e2a98956dc896
Doug Franklin <doug@dougfranklin.com>
2021-02-28 10:16:57 -0500

    Reload and devTools globalShortcut(s)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
 
 // Set environment
 process.env.NODE_ENV = 'development';
@@ -37,6 +37,14 @@ app.on('ready', () => {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
+  globalShortcut.register('CmdOrCtrl+R', () => {
+    mainWindow.reload();
+  });
+
+  globalShortcut.register('CmdOrCtrl+I', () => {
+    mainWindow.toggleDevTools();
+  });
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   })
@@ -48,16 +56,16 @@ const menu = [
   ...(isMac ? [{ role: 'appMenu' }] : []),
 
   {
+    label: 'File',
+    submenu: [{
+      label: 'Quit',
+      // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+      accelerator: 'CmdOrCtrl+W',
+      click: () => app.quit()
+    }]
 
-  label: 'File',
-  submenu: [{
-    label: 'Quit',
-    // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
-    accelerator: 'CmdOrCtrl+W',
-    click: () => app.quit()
-  }]
-
-}];
+  }
+];
 
 // if (isMac) {
 //   menu.unshift({ role: 'appMenu' });
